92e5a7f606e6fab6138d8393dbe4e7888a54a664
MATEO San Miguel Cabrera <MS260864@falmouth.ac.uk>
2022-11-11 17:35:20 +0000

    made cactus and started working on spikes

---
+++ Assets/Cactus.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cactus : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D spike;
+    public Transform[] shootDirs;
+
+    private void Awake()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        foreach (var shootDir in shootDirs)
+        {
+            Shoot(shootDir);
+        }
+
+        Destroy(gameObject);
+    }
+
+    void Shoot(Transform currentDir)
+    {
+        //Vector2 dir = (transform.position - currentDir.position).normalized;
+        Rigidbody2D newSpike = Instantiate(spike, transform.position, Quaternion.identity);
+        //newSpike.transform.right = currentDir.position - newSpike.transform.position;
+
+        float angle = Mathf.Atan2(currentDir.position.y, currentDir.position.x) * Mathf.Rad2Deg;
+        newSpike.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
---
+++ Assets/Spike.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+
+    [SerializeField] int speed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(transform.up * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
